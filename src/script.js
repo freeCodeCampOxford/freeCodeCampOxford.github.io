1db9e558ed7f47e72c374f7fdd363d703f118924
Jamie Bowman <jamiereecebowman@gmail.com>
2018-09-13 23:01:09 +0100

    cycle active class thru navlinks on page scroll down

--- src/script.js
+++ src/script.js
@@ -17,5 +17,56 @@ $(document)
     // create sidebar and attach to menu open
     $('.ui.sidebar')
     .sidebar('attach events', '.toc.item');
+    
+    $('h1.ui.inverted.header')
+    .visibility({
+        once: false,
+        onTopVisible: function() {
+            $('.home-link-item').addClass('active');
+        }
+    })
+    
+    $('#aboutfcc')
+        .visibility({
+            once: false,
+            onBottomVisible: function() {
+                $('.fcc-link-item').addClass('active');
+            },
+            onBottomPassed: function() {
+                $('.fcc-link-item').removeClass('active');
+            }
+        });
 
+    $('#aboutus')
+        .visibility({
+            once: false,
+            onTopPassed: function() {
+                $('.fcc-link-item').removeClass('active');
+                $('.aboutus-link-item').addClass('active');
+            },
+            onPassingReverse: function() {
+                $('.meetups-link-item').removeClass('active');
+            }
+        });
+
+    $('#meetups')
+        .visibility({
+            once: false,
+            onBottomVisible: function() {
+                $('.aboutus-link-item').removeClass('active');
+                $('.meetups-link-item').addClass('active');
+            }
+        });
+
+    $('.footer.segment')
+        .visibility({
+            once: false,
+            onTopVisible: function() {
+                $('.meetups-link-item').removeClass('active');
+                $('.ourpages-link-item').addClass('active');
+            },
+            onTopVisibleReverse: function() {
+                $('.ourpages-link-item').removeClass('active');
+            }
+        });
 });
